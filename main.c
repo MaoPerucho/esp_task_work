578ae6ac81b5bdbf5f4d16e90d282a8eda8023ea
Mauricio <hedinyer.perucho@ustabuca.edu.co>
2021-03-11 17:48:47 -0500

    primero

--- main.c
+++ main.c
@@ -3,7 +3,37 @@
 #include <freertos/task.h>
 #include <esp_system.h>
 
+void tarea4 (void *pvParameter)
+{
+    While (true)
+    {
+        GreenStart1=1;
+        RedStart2=1;
+        RedStart1=0;
+        GreenStart2=0;
+        
+        if (time.s=>Total.time)
+        {
+           GreenStart2 = 1;
+           RedStart1=1;
+           RedStart2=0;
+           GreenStart1=0;
+           time.s=0;
+        }
 
+        if (PedMode==1 & time.s=>Total.time)
+        {
+           GreenStart2=0;
+           RedStart1=1;
+           RedStart2=1;
+           GreenStart1=0;
+           zumbador=1;
+           ets_delay_s(10);
+           zumbador=0;
+           PedMode=0;
+        }
+    }
+}
 
 void app_main()
 {
