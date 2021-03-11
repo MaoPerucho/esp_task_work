1bdf86f3ac669891291c6fa41b87e51daf15d372
Mauricio <hedinyer.perucho@ustabuca.edu.co>
2021-03-11 17:52:28 -0500

    tercero

--- main.c
+++ main.c
@@ -3,6 +3,32 @@
 #include <freertos/task.h>
 #include <esp_system.h>
 
+
+void task_1(void)
+{
+	while (true)
+	{
+	if (R1_b=true && Amb1_b=true)
+	{
+		printf("NORTE_str VERDE");
+	}
+if (Ambr_b=true && V1_b=true)
+	{
+		printf("NORTE_str ROJO");
+	}
+if (R2_b=true && Amb2_b=true)
+	{
+		printf("ESTE_str VERDE");
+	}
+if (V2_b=true && Amb2_b=true)
+	{
+		printf("ESTE_str ROJO");
+	}
+
+	}
+}
+
+
 void tarea4 (void *pvParameter)
 {
     While (true)
