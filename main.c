75e94643445548f959011faa96fdd96a7d3f94a7
MaoPerucho <70913789+MaoPerucho@users.noreply.github.com>
2021-03-11 17:33:40 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,71 @@
-#include <driver/gpio.h>
-#include <freertos/FreeRTOS.h>
-#include <freertos/task.h>
-#include <esp_system.h>
-
-
-
-void app_main()
+void controlleds()
 {
-    
+	while(true)
+	{
+		Cnt=descontar(Cnt);
+		Cnt2=descontar(Cnt2);
+		
+		if( RedStart1==1)
+		{
+			Cnt=RedDuration1;
+			RedStart1=0;
+			
+		}
+		else
+		{
+		if( GreenStart1==1)
+		{
+			Cnt=GreenDuration1;
+			GreenStart1=0;
+			
+		}
+		else
+		{
+		
+		if( RedStart2==1)
+		{
+			Cnt2=RedDuration2;
+			RedStart2=0;
+			
+		}
+		else
+		{
+			if( GreenStart2==1)
+			{
+				Cnt2=GreenDuration2;
+				GreenStart2=0;
+			}
+		}
+		}
+		}
+	}
+		
+		
+	}
+	
+	
+	
+	
+}
+int descontar(int Cnt)
+{
+	
+	if(Cnt!=99)
+	{
+		if(Cnt==0)
+	
+		{
+		  Cnt=99;
+		
+		}	
+		else
+		{
+			delay(1000);
+			Cnt=Cnt-1;
+		}
+		
+	}
+	return Cnt;
+	
+	
 }
